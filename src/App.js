ac9745d18d4116fdc3f21f3efb92375ab90d56b6
Abhishek Rana <abhishek@Abhisheks-MacBook-Pro.local>
2020-08-01 12:30:19 +0530

    Initial setup.

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Appsync and apollo libraries
+import AWSAppSyncCLient from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react'
+import ApolloProvider from 'react-apollo';
+
+// Amplify
+import Amplify, { Auth } from 'aws-amplify';
+
+// Components
+import awsconfig from './aws-exports';
+
+// Amplify init
+Amplify.configure(awsconfig);
+
+const GRAPHQL_API_REGION = awsconfig.aws_appsync_region;
+const GRAPHQL_API_ENDPOINT_URL = awsconfig.aws_appsync_graphqlEndpoint;
+const S3_BUCKET_REGION = awsconfig.aws_user_files_s3_bucket_region;
+const S3_BUCKET_NAME = awsconfig.aws_user_files_s3_bucket;
+const AUTH_TYPE = awsconfig.aws_appsync_authenticationType;
+
+// AppSync client instantiation
+const client = new AWSAppSyncClient({
+  url: GRAPHQL_API_ENDPOINT_URL,
+  region: GRAPHQL_API_REGION,
+  auth: {
+    type: AUTH_TYPE,
+    jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken()
+  },
+  complexObjectsCredentials: () => Auth.currentCredentials()
+})
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
